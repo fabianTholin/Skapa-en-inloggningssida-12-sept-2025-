bcbe1b7a383c61a13eca7caf0dbf68349bc77a13
fabianTholin <thfa25ko@student.ju.se>
2025-09-11 18:15:06 +0200

    Justerat användarnamnInput samt lösenordInput.

--- script.js
+++ script.js
@@ -20,24 +20,26 @@ function misslyckadInloggning(){
 
 function testaLoggaIn(){
 
-    let användarnamn = document.getElementById("användarnamn").value;
-    let lösenord = document.getElementById("lösenord").value;
+    let användarnamn = document.getElementById("användarnamnInput").value;
+    let lösenord = document.getElementById("lösenordInput").value;
     let rättInloggningsuppgifter = Boolean(false);
     document.getElementById("start").hidden = true;
     
     if (användarnamn == "test" && lösenord == "1234"){
         rättInloggningsuppgifter = "true";
         localStorage.setItem("username", användarnamn);
-        localStorage.setItem("password", "lösenord");
+        localStorage.setItem("password", lösenord);
         lyckadInloggning();
     }
     else misslyckadInloggning();
 }
 
 function loggaUt(){
-    document.getElementById("användarnamn").value = ""; //Här kunde en "reset"-funktion användas (?)
-    document.getElementById("lösenord").value = "";
+    document.getElementById("användarnamnInput").value = ""; //Här kunde en "reset"-funktion användas (?)
+    document.getElementById("lösenordInput").value = "";
     document.getElementById("start").hidden = false;
     document.getElementById("inloggad").hidden = true;
     localStorage.clear();
+    console.log(username);
+    console.log(password);
     }
