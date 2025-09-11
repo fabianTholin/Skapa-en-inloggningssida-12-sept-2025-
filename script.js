69a87bf033a880972f7b9a586007875beafc01c4
fabianTholin <thfa25ko@student.ju.se>
2025-09-11 13:36:49 +0200

    G´-krav uppfyllda

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const rättAnvändarnamn = "test";
 const rättLösenord = "1234";
 
+document.getElementById("inloggad").hidden = true;
+document.getElementById("fel").hidden= true;
+
 function lyckadInloggning(){
-    document.getElementById("start").style.display = "none";
+    document.getElementById("inloggad").hidden = false;
 }
 
-function testaLoggaIn (){
+function misslyckadInloggning(){
+    document.getElementById("fel").hidden= false;
+}
+
+function testaLoggaIn(){
 
     let användarnamn = document.getElementById("användarnamn").value;
     let lösenord = document.getElementById("lösenord").value;
     let rättInloggningsuppgifter = Boolean(false);
-
+    document.getElementById("start").hidden = true;
+    
     if (användarnamn == "test" && lösenord == "1234"){
         rättInloggningsuppgifter = "true";
         lyckadInloggning();
     }
-    else rättInloggningsuppgifter = "false";
+    else misslyckadInloggning();
     
     console.log ("Användarnamn: " + användarnamn);
     console.log ("Lösenord: " + lösenord);
     console.log ("Rätt inloggningsuppgifter? = " + rättInloggningsuppgifter);
 }
 
+function loggaUt(){
+    localStorage.clear();
+    /*document.getElementById("användarnamn").value = ""; //Här kunde en "reset"-funktion användas (?)
+    document.getElementById("lösenord").value = "";
+    document.getElementById("start").hidden = false;
+    document.getElementById("inloggad").hidden = true; */
+}
