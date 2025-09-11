5087fcc48c23a7e5df94d2591b0441912a8beb59
fabianTholin <thfa25ko@student.ju.se>
2025-09-11 14:17:58 +0200

    Klar, enligt VG-krav (tror jag)

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const rättAnvändarnamn = "test";
 const rättLösenord = "1234";
+const username = localStorage.getItem("username");
+const password = localStorage.getItem("password");
 
 document.getElementById("inloggad").hidden = true;
 document.getElementById("fel").hidden= true;
 
 function lyckadInloggning(){
     document.getElementById("inloggad").hidden = false;
-}
+    const username = localStorage.getItem("username");
+    const password = localStorage.getItem("password");
+    console.log(username);
+    console.log(password);
+   }
 
 function misslyckadInloggning(){
     document.getElementById("fel").hidden= false;
@@ -21,19 +27,17 @@ function testaLoggaIn(){
     
     if (användarnamn == "test" && lösenord == "1234"){
         rättInloggningsuppgifter = "true";
+        localStorage.setItem("username", användarnamn);
+        localStorage.setItem("password", "lösenord");
         lyckadInloggning();
     }
     else misslyckadInloggning();
-    
-    console.log ("Användarnamn: " + användarnamn);
-    console.log ("Lösenord: " + lösenord);
-    console.log ("Rätt inloggningsuppgifter? = " + rättInloggningsuppgifter);
 }
 
 function loggaUt(){
-    localStorage.clear();
-    /*document.getElementById("användarnamn").value = ""; //Här kunde en "reset"-funktion användas (?)
+    document.getElementById("användarnamn").value = ""; //Här kunde en "reset"-funktion användas (?)
     document.getElementById("lösenord").value = "";
     document.getElementById("start").hidden = false;
-    document.getElementById("inloggad").hidden = true; */
-}
+    document.getElementById("inloggad").hidden = true;
+    localStorage.clear();
+    }
